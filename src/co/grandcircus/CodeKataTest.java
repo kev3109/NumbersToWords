e8b5c18445d70f15505e84e561f10e9a6edc5fa3
Kevon Butler <kevbutler310@gmail.com>
2019-08-14 09:16:13 -0400

    NumbersToWords Help Me

---
+++ src/co/grandcircus/CodeKataTest.java
@@ -0,0 +1,224 @@
+package co.grandcircus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CodeKataTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test1() {
+		CodeKata numWords = new CodeKata();
+		String expected = "one";
+		String actual = numWords.numberToWord(1);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test2() {
+		CodeKata numWords = new CodeKata();
+		String expected = "two";
+		String actual = numWords.numberToWord(2);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test3() {
+		CodeKata numWords = new CodeKata();
+		String expected = "three";
+		String actual = numWords.numberToWord(3);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test4() {
+		CodeKata numWords = new CodeKata();
+		String expected = "four";
+		String actual = numWords.numberToWord(2);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test5() {
+		CodeKata numWords = new CodeKata();
+		String expected = "five";
+		String actual = numWords.numberToWord(5);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test6() {
+		CodeKata numWords = new CodeKata();
+		String expected = "six";
+		String actual = numWords.numberToWord(6);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test7() {
+		CodeKata numWords = new CodeKata();
+		String expected = "seven";
+		String actual = numWords.numberToWord(7);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test8() {
+		CodeKata numWords = new CodeKata();
+		String expected = "eight";
+		String actual = numWords.numberToWord(8);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test9() {
+		CodeKata numWords = new CodeKata();
+		String expected = "nine";
+		String actual = numWords.numberToWord(9);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test10() {
+		CodeKata numWords = new CodeKata();
+		String expected = "ten";
+		String actual = numWords.numberToWord(10);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test11() {
+		CodeKata numWords = new CodeKata();
+		String expected = "eleven";
+		String actual = numWords.numberToWord(11);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test12() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twelve";
+		String actual = numWords.numberToWord(12);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test13() {
+		CodeKata numWords = new CodeKata();
+		String expected = "thirteen";
+		String actual = numWords.numberToWord(13);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test14() {
+		CodeKata numWords = new CodeKata();
+		String expected = "fourteen";
+		String actual = numWords.numberToWord(14);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test15() {
+		CodeKata numWords = new CodeKata();
+		String expected = "fifteen";
+		String actual = numWords.numberToWord(15);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test16() {
+		CodeKata numWords = new CodeKata();
+		String expected = "sixteen";
+		String actual = numWords.numberToWord(16);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test17() {
+		CodeKata numWords = new CodeKata();
+		String expected = "seventeen";
+		String actual = numWords.numberToWord(17);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test18() {
+		CodeKata numWords = new CodeKata();
+		String expected = "eighteen";
+		String actual = numWords.numberToWord(18);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test19() {
+		CodeKata numWords = new CodeKata();
+		String expected = "nineteen";
+		String actual = numWords.numberToWord(19);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test20() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twenty";
+		String actual = numWords.numberToWord(20);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test21() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twentyone";
+		String actual = numWords.numberToWord(21);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test22() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twentytwo";
+		String actual = numWords.numberToWord(22);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test23() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twentythree";
+		String actual = numWords.numberToWord(23);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test24() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twentyfour";
+		String actual = numWords.numberToWord(24);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test25() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twentyfive";
+		String actual = numWords.numberToWord(25);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test26() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twentysix";
+		String actual = numWords.numberToWord(26);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test27() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twentyseven";
+		String actual = numWords.numberToWord(27);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test28() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twentyeight";
+		String actual = numWords.numberToWord(28);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test29() {
+		CodeKata numWords = new CodeKata();
+		String expected = "twentynine";
+		String actual = numWords.numberToWord(29);
+		assertEquals(expected, actual);
+	}
+	@Test
+	void test30() {
+		CodeKata numWords = new CodeKata();
+		String expected = "thirty";
+		String actual = numWords.numberToWord(30);
+		assertEquals(expected, actual);
+	}
+}
---
+++ src/co/grandcircus/NumbersToWords.java
@@ -0,0 +1,24 @@
+package co.grandcircus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NumbersToWords {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test1() {
+		CodeKata numWords = new CodeKata();
+		int expected = 1;
+		String actual = numWord.;
+		assertEquals(expected, actual);
+	}
+
+	
+
+}
